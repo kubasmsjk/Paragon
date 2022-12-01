68e2ea2b76c504211930182e3098120ae0eae30f
JakubMieczkowski(109 <jakm5000@wp.pl>
2022-12-01 15:38:03 +0100

    delete and edit

--- receipt.js
+++ receipt.js
@@ -5,7 +5,7 @@ var receipt = new Receipt();
 var button = document.getElementById('btn');
 var sum = document.getElementById('sum');
 var table = document.getElementById('table').getElementsByTagName('tbody')[0];
-var numberOfRow = 0;
+var icon = document.getElementById('bi');
 
 button.addEventListener("click", function(){
 
@@ -14,28 +14,66 @@ button.addEventListener("click", function(){
     let price = document.getElementById('priceOfProduct').value;
 
     let product = new Product(name,price,amount);
-    let checkIsProductInList = receipt.addProduct(product);
+    let checkProductIsNotInList = receipt.addProduct(product);
+    if(checkProductIsNotInList){
+        saveToLocalStorage();
+   }
+})
 
-    if(checkIsProductInList){
 
-        let row = table.insertRow(numberOfRow);
-        numberOfRow++;
 
+
+function printArray(receipt){
+
+    for (var i = 0; i < receipt._listOfProducts.length; i++) {
+        
+        let row = table.insertRow(i);
+        row.addEventListener("click", function(event) {
+                if( event.target.id == 'bi' ){
+                    receipt.deleteProduct(receipt._listOfProducts[row.id]);
+                    saveToLocalStorage();
+                    window.location.reload();
+                }
+                if(!event.target.id){
+                    event.target.innerHTML = "<input type ='text' placeholder =" + event.target.textContent+"></input>"
+                }
+               
+            })
+            
+
+        row.id = i;
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell(1);
         let cell3 = row.insertCell(2);
         let cell4 = row.insertCell(3);
         let cell5 = row.insertCell(4);
-
-        cell1.innerHTML = numberOfRow;
-        cell2.innerHTML = name;
+        let cell6 = row.insertCell(5);
+        let amount = receipt._listOfProducts[i]._amount;
+        let price = receipt._listOfProducts[i]._price
+        cell1.id = i;
+        cell1.innerHTML = i;
+        cell2.innerHTML = receipt._listOfProducts[i]._name;
         cell3.innerHTML = amount;
         cell4.innerHTML = price + " zł";
         cell5.innerHTML = Math.floor((amount*price) *100) / 100 + " zł";
-        sum.innerHTML = parseInt(sum.textContent) + Math.floor((amount*price) *100) / 100;
+        cell6.innerHTML='<i id="bi" class="bi bi-x-octagon"></i>'
+        sum.innerHTML = parseInt(sum.textContent) + Math.floor((amount*price) *100) / 100 + " zł";
     }
 
-    
-})
-//var newText = document.createTextNode('new row');
-//newCell.appendChild(newText);
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem("receipt",JSON.stringify(receipt));
+ }
+
+function getFromLocalStorage() {
+    receipt._listOfProducts = JSON.parse(localStorage.getItem("receipt") || "[]")._listOfProducts;
+}
+
+window.addEventListener("load", function() {
+    //window.localStorage.clear();
+    if(localStorage.length > 0 ) {
+        getFromLocalStorage();
+        printArray(receipt);
+    }
+  });
